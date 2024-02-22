d622ba75882f4ae2f7b7bf719aae6fcab18e99f9
chiyoi <chiyoi2140@outlook.com>
2024-02-23 00:43:04 +0800

    tidy

--- Stages/Conclusion/conclusion.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-func _on_control_gui_input(event: InputEvent):
-	if (not event is InputEventMouseButton or
-			not event.is_pressed() or
-			event.button_index != MOUSE_BUTTON_LEFT):
-		return
-	get_tree().quit()
