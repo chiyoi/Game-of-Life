2393d58b8971358147150909997242203c8ccda7
chiyoi <chiyoi2140@outlook.com>
2024-02-23 00:39:30 +0800

    update

--- Stages/Conclusion/conclusion.gd
+++ Stages/Conclusion/conclusion.gd
@@ -1,4 +1,8 @@
 extends Node
 
-func _on_quit_game_pressed():
+func _on_control_gui_input(event: InputEvent):
+	if (not event is InputEventMouseButton or
+			not event.is_pressed() or
+			event.button_index != MOUSE_BUTTON_LEFT):
+		return
 	get_tree().quit()
